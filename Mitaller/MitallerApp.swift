b9c10b5ab49c383e3ffc4ec7f4df040b4ec3155a
Angel velasco <Angel@speedwear.es>
2026-05-05 10:43:32 +0200

    Initial Commit

---
+++ Mitaller/MitallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MitallerApp.swift
+//  Mitaller
+//
+//  Created by Angel Velasco on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MitallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
